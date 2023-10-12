3dccc2143d6c6b13f475cc9b6c0812a24b4f5d1a
Nebbilies <dmhieutrung@gmail.com>
2023-10-12 20:56:05 +0700

    Updated /about page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {
-      fontFamily: {
-        comicsans: ['Comic Sans MS', 'Comic Sans', 'cursive'],
-      }
+    content: ['./src/**/*.{html,js,svelte,ts}'],
+    theme: {
+        extend: {
+            fontFamily: {
+                comicsans: ['Comic Sans MS', 'Comic Sans', 'cursive'],
+                mplus: ['"M PLUS Rounded 1c"', 'sans-serif'],
+            }
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
 
